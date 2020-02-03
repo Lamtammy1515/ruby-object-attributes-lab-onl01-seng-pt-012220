7018a081958188231d8fdf76a69c0f45835782ce
Tammy Lam <lamtammy1515@gmail.com>
2020-02-03 21:27:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,7 @@ class Dog
   end
     
   def name
-    "#{name}"
+    "#{dog_name}"
   end 
 
   def breed=(breed_name)
@@ -13,7 +13,7 @@ class Dog
 end 
 
   def breed 
-    "#{breed}"
+    "#{breed_name}"
   end 
   
 end 
