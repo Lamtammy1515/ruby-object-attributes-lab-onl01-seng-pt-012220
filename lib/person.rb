6d93c31d72cfa9a4faa901f8d0d8f6a9187b5eee
Tammy Lam <lamtammy1515@gmail.com>
2020-02-03 21:26:55 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,13 +1,10 @@
 class Person 
-  @@all = []
+
   def initialize(name)
     @name = name 
-    @@all << self 
+
   end 
   
-  def self.all
-    @@all 
-  end 
   
   def job=(job_name)
     @job = job_name
