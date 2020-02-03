3b55fa73811474069ec18b73e9399833bcd6ad2e
Tammy Lam <lamtammy1515@gmail.com>
2020-02-03 21:01:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,13 @@
 class Dog 
-  @@all = []
+
   
   def initialize(name, breed)
     @name = name
-    @@all << self 
   end 
-  
-  def self.all 
-    @@all 
-  end 
+
   
   def name
     @name 
-end 
+  end 
 
 end 
