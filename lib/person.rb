e06593be5b27c507356397ae9e339b2f3b15389e
Tammy Lam <lamtammy1515@gmail.com>
2020-02-03 21:30:30 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,19 @@
 class Person 
   
   def name=(full_name)
-    @name = 
+    @name = full_name 
+  end 
+  
+  def name 
+    @name
+  end 
+  
+  def job=(job_name)
+    @job = job_name
+  end 
+  
+  def job
+    @job
+  end 
+  
+end 
